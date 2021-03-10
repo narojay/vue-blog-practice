87232253375106c3405e8db73e917ac797018726
narojay <hj200812@126.com>
2021-03-10 22:12:09 +0800

    Feature 添加注释

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -29,6 +29,7 @@ export default {
 </script>
 <style>
 #app {
+  /* 去除边框*/
   position: absolute;
   top: 0;
   left: 0;
@@ -47,6 +48,14 @@ export default {
   color: #333;
   text-align: center;
   line-height: 100vh;
+  /* 
+   vh就是当前屏幕可见高度的1%，也就是说
+
+height:100vh == height:100%;
+
+但是有个好处是当元素没有内容时候，设置height:100%该元素不会被撑开，
+
+但是设置height:100vh，该元素会被撑开屏幕高度一致。*/
 }
 
 .el-main {
